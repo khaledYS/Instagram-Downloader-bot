5c2c90c2b03f4edb23d3f230bab532b8fba52538
ammar <ammar@gmail.com>
2024-05-17 19:23:05 +0300

    updated

--- bot-commands.js
+++ bot-commands.js
@@ -1,10 +1,13 @@
-const { ttdl } = require("btch-downloader");
+const igdl = require("instagram-url-direct");
 const { Telegraf, Markup } = require("telegraf");
 require("dotenv").config();
+
 const token = process.env.TOKEN;
 const chnl_token = process.env.CHNL;
 const bot = new Telegraf(token);
 const joiningMessage = "Please Join our Channel first 🤍🤍 \n انضم الى قنات البوت اولاً 🤍🤍\n \n - https://t.me/gkcl_store \n - https://t.me/gkcl_store"
+const poweredBy = "POWERED BY:@OxGkcl";
+
 const onlyJoined = (callback) => async (ctx) => {
     const client_id = ctx.update.message.from.id;
     const res = await ctx.telegram.getChatMember(chnl_token, client_id);
@@ -22,27 +25,40 @@ bot.command("start", onlyJoined((ctx) => {
 bot.on("callback_query", async (ctx) => {
     const query = ctx.update.callback_query.data;
     const [option, ...data] = query.split("|");
+    // ctx.telegram.editMessageMedia()
 })
 
 bot.on("message", onlyJoined(async (ctx) => {
     const From = ctx.update.message.from.id;
-    const body = /^https:\/\/.*tiktok\.com\/.+/;
+
+    let msg = (await ctx.reply(" Fetching your request..."));
+    messageId = msg.message_id;
+    chatId = msg.chat.id;
+
     const url = ctx.update.message.text;
-    if (body.test(url)) {
-        try {
-            const data = await ttdl(url);
-            const audio = data.audio[0]
-            const { title, title_audio } = data;
-            await ctx.sendVideo(data.video[0], {chat_id: From, caption: title });
-            await ctx.sendAudio(data.audio[0], { chat_id: From,caption: title_audio });
-            await ctx.sendMessage( 'Powered by @oxgkcl', {chat_id: From});
-        } catch (error) {
-            ctx.sendMessage('Sorry, an error occurred while downloading the TikTok video.', {chat_id: From});
+    try {
+        const data = await igdl(url);
+        console.log("hhhhhhhhhhhh")
+        if (data.results_number >= 1) {
+            const res = data.url_list.map((url, _ind) => {
+                return {
+                    type: "video",
+                    media: url
+                }
+            });
+            await ctx.telegram.editMessageText(chatId, messageId, undefined, "Done");
+            await ctx.telegram.deleteMessage(chatId, messageId)
+            await ctx.sendChatAction("upload_video");
+            await ctx.telegram.sendMediaGroup(chatId, res);
+            await ctx.telegram.sendMessage(chatId, ("Done, " + poweredBy))
         }
-    }else { 
-        ctx.sendMessage("عذراً, يرجى ارسال رابط صحيح \n Sorry, must send a valid link")
+    } catch (error) {
+        await ctx.telegram.editMessageText(chatId, messageId, undefined, 'Invalid Link, Please provide valid links. \n الرابط غير صحيح \n \n POWERED BY:@OxGkcl')
+        console.log(error)
     }
-}));
+})
+
+);
 
 bot.catch(async (err, ctx) => {
     console.log(`Ooops, encountered an error for ${ctx.updateType}`, err)
